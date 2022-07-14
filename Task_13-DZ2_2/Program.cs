ff8ce2734eeadee39b03c753283a354bddb3c69d
Nataly Suvorova <suvorova.nata.a@gmail.com>
2022-07-14 18:59:10 +0300

    ДЗ-2. Задача 2. Вывод 3-ей цифры числа

---
+++ Task_13-DZ2_2/Program.cs
@@ -0,0 +1,37 @@
+﻿// Вывести трьетью цифру числа, или сообщить, что третьей цифры нет
+// баг - масимум можно ввести 9 цифр
+
+int kolichestvo(int x) // Метод - определяю количество цифр в числе. 
+{
+    int count = 0;
+    while (x > 0)
+    {
+        x = x / 10;
+        count++;
+    }
+    Console.WriteLine($"В числе {count} цифр");
+    return count;
+}
+
+Console.WriteLine("Введите число");
+string? inNumber = Console.ReadLine();
+
+if (inNumber != null)
+{
+    int number = int.Parse(inNumber);
+    int count = kolichestvo(number); // вызываю метод
+    if (count == 3)   // если в числе 3 цифры
+    {
+        int numberShort = number % 10;
+        Console.WriteLine("Третья цифра - " + numberShort);
+    }
+    else if (count > 3)
+    {
+        double y = number / Math.Pow(10, (count - 3)); // отбрасываю хвост числа. оставляю 3 цифры
+       double numberShort = Math.Floor(y); // округляю число до целого в меньшую сторону
+       // int numberShort = (int)y;  // так тоже можно отбросить числа после запятой
+        Console.WriteLine("Третья цифра - " + numberShort % 10);
+    }
+    else Console.WriteLine("В числе нет третьей цифры"); 
+
+}
