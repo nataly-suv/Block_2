3150a7521631651a39c2162f7f9b58462626b9c6
Nataly Suvorova <suvorova.nata.a@gmail.com>
2022-07-14 19:06:47 +0300

    ДЗ-2. Задача 3. Определяем выходной

---
+++ Task_14-DZ2_3/Program.cs
@@ -0,0 +1,16 @@
+﻿// определяем выходной 
+Console.WriteLine("Введите число от 1 до 7");
+string? inNumber = Console.ReadLine();
+
+if (inNumber != null)
+{
+    int number = int.Parse(inNumber);
+    if ((0 < number) && (number < 8))
+    {
+        if ((number == 6) || (number == 7)) Console.WriteLine("Ура! Выходной");
+        else Console.WriteLine("это не выходной");
+    }
+    else Console.WriteLine("простите, мы не можем определить день недели");
+}
+Console.WriteLine();
+
