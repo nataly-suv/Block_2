2b290527c5757ec236082bc1b21a5b6e9d367a57
Nataly Suvorova <suvorova.nata.a@gmail.com>
2022-07-14 18:04:51 +0300

    ДЗ-1. Тернальная операция

---
+++ Task_01-DZ_1-Changes_1/Program.cs
@@ -0,0 +1,15 @@
+﻿// Поиск максимального из 2-х. Тернарная операция.
+Console.WriteLine("Введите первое число");
+string? inNumber1 = Console.ReadLine();
+Console.WriteLine("Введите второе число");
+string? inNumber2 = Console.ReadLine();
+if (inNumber1 != null && inNumber2 != null)
+{
+    int number1 = int.Parse(inNumber1);
+    int number2 = int.Parse(inNumber2);
+    int result = number1 > number2 ? number1 : number2; // тернальный оператор. 
+    // если условие (number1>number2) true (выполняется), то в result записываем number1, 
+    // если условие не выполняется, то в result записывает number2. 
+    Console.Write($"Максимальное число - {result}");
+}
+
