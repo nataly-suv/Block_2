f2d3547e414984ac2eaf0ac04374923e06e9475b
Nataly Suvorova <suvorova.nata.a@gmail.com>
2022-07-14 18:47:24 +0300

    Семинар 2. Задача 5. Кратность на 7 и 23

---
+++ Task_11-Sem2_5/Program.cs
@@ -0,0 +1,12 @@
+﻿//Проверяем кратно ли число одновременно 7 и 23
+
+Console.WriteLine("Введите число");
+string? inNum = Console.ReadLine();
+
+if (inNum != null)
+{
+    int num = int.Parse(inNum);
+    if ((num % 7 == 0) && (num % 23 == 0)) Console.WriteLine("YES");
+    else Console.WriteLine("NO");
+}
+
