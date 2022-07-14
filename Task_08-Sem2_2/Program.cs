020309f8272a18ebc162f887751c1679297256de
Nataly Suvorova <suvorova.nata.a@gmail.com>
2022-07-14 18:37:29 +0300

    Семинар 2. Задача 2. Методы. Удаление 2-ой цифры

---
+++ Task_08-Sem2_2/Program.cs
@@ -0,0 +1,27 @@
+﻿// Удаление 2-ой цифры в 3-х значном числе
+
+void variant1()
+{
+    int number = new Random().Next(100, 1000);
+    Console.WriteLine("исходное число: " + number);
+
+    int num1 = number / 100;
+    int num3 = number % 10;
+    int result = num1 * 10 + num3;
+    Console.WriteLine("ответ: " + result);
+}
+
+void variant2()
+{
+    int number = new Random().Next(100, 1000);
+    Console.WriteLine("исходное число: " + number);
+    string numString = number.ToString();
+    Console.Write("ответ:  ");
+    Console.Write(numString[0]);
+    Console.Write(numString[2]);
+}
+
+variant1();
+Console.WriteLine();
+variant2();
+
