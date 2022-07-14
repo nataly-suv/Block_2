a429a9904efa9547a3e83d585409afbde8b96fb7
Nataly Suvorova <suvorova.nata.a@gmail.com>
2022-07-14 18:10:21 +0300

    ДЗ-1. Поиск мах. Math

---
+++ Task_02-DZ_1_Changes_2/Program.cs
@@ -0,0 +1,17 @@
+﻿// Поиск максимального из трех с помощью Math класса.
+Console.WriteLine("Введите первое число");
+int number1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int number2 = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите третье число");
+int number3 = int.Parse(Console.ReadLine());
+
+int resultMiddle = Math.Max(number1, number2);
+int result = Math.Max(number3, resultMiddle);
+
+//int result = Math.Max(number1, Math.Max(number2, number3)); 
+// это подсмотрено. но себе сохраню, как клевый алгоритм и что так можно
+
+Console.WriteLine($"Максимальное число - {result}");
+
+
