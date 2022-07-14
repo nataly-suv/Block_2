07f6083b4e20d538635757624a2f62475454739b
Nataly Suvorova <suvorova.nata.a@gmail.com>
2022-07-14 18:18:08 +0300

    Лекция 2. Задача 2. Поиск max. Массив. Методы

---
+++ Task_04-L2_2/Program.cs
@@ -0,0 +1,20 @@
+﻿// Массив. Поиск максимального в массиве с помощью метода (функции)
+
+int Max(int arg1, int arg2, int arg3)                   // создаем метод (функцию) для поиска max из трех
+{                                          // тело метода (функции)  
+    int result = arg1;
+    if (arg2 > result) result = arg2;
+    if (arg3 > result) result = arg3;
+    return result;
+}
+// индексы      0   1  2    3   4   5   6   7   8
+int[] array = { 1001, 54, 66, 43, 89, 27, 123, 3, 63 };   // создали массив с типом int, c именем array
+
+int max = Max(                                       // вызываем метод (функцию)
+    Max(array[0], array[1], array[2]),
+    Max(array[3], array[4], array[5]),
+    Max(array[6], array[7], array[8])
+);
+
+Console.WriteLine($"Максимальный элемент массива -  {max}");
+
