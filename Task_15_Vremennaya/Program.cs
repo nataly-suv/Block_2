47cd22ad09253f5b5a4d1ddd930ac3e6c8f195b7
Nataly Suvorova <suvorova.nata.a@gmail.com>
2022-07-30 12:46:08 +0300

    njh

--- Task_15_Vremennaya/Program.cs
+++ Task_15_Vremennaya/Program.cs
@@ -1,60 +1,27 @@
-﻿// Вывести случайное число [10. 100] и показать наибольшую цифру числа
+﻿// Удаление 2-ой цифры в 3-х значном числе
 
-// вариант 1
-void variant1(System.Random numberSint)
-// в метод передаю переменную numberSint
+void variant1()
 {
-    int number = numberSint.Next(10, 100);
-    // вызываю метод Next для определя случайного числа [10, 100)
-    int firstNumber = number / 10;
-    int secondNumber = number % 10;
-    Console.WriteLine(number);
+    int number = new Random().Next(100, 1000);
+    Console.WriteLine("исходное число: " + number);
 
-    if (firstNumber == secondNumber)
-        Console.WriteLine("Цифры в числе одинаковые и равны - " + firstNumber);
-    else if (firstNumber > secondNumber)
-        Console.WriteLine("Максимальная цифра в цисле - " + firstNumber);
-    else Console.WriteLine("Максимальная цифра в цисле - " + secondNumber);
-    Console.WriteLine();
+    int num1 = number / 100;
+    int num3 = number % 10;
+    int result = num1 * 10 + num3;
+    Console.WriteLine("ответ: " + result);
 }
 
-// вариант 2 
 void variant2()
 {
-    int number2 = new Random().Next(10, 100);
-    // более короткий способ определяни случайного числа
-    Console.WriteLine("случайное число - " + number2);
-    string stringNum = number2.ToString(); //перевожу число в строку
-
-    if (stringNum[0] == stringNum[1])
-        Console.WriteLine("Цифры равны и имеют значение " + stringNum[0]);
-    else if (stringNum[0] > stringNum[1])
-        Console.WriteLine("Максимальная цифра - " + stringNum[0]);
-    else Console.WriteLine("Максимальная цифра - " + stringNum[1]);
-    Console.WriteLine();
+    int number = new Random().Next(100, 1000);
+    Console.WriteLine("исходное число: " + number);
+    string numString = number.ToString();
+    Console.Write("ответ:  ");
+    Console.Write(numString[0]);
+    Console.Write(numString[2]);
 }
 
-// вариант 3
-void variant3()
-{
-    System.Random number3 = new Random();
-
-    char[] mas = number3.Next(10, 100).ToString().ToCharArray();
-    //          случайное чило переводим в строку и переводим в массив   
-
-    Console.WriteLine(mas);
-
-    int firstNum = ((int)mas[0]) - 48;
-    int secondNum = ((int)mas[1]) - 48;
-
-    // далее тернальный опрератор 
-    int result = firstNum > secondNum ? result = firstNum : result = secondNum;
-    Console.WriteLine("Максимальная цифра числа - " + result);
-}
-
-Console.Clear();
-System.Random numberSint = new Random();  //определяю класс переменной numberSint
-
-variant1(numberSint);
+variant1();
+Console.WriteLine();
 variant2();
-variant3();
+
